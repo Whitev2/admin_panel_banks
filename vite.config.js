714acdbdeaca1413c17161d756e4ed64abfa165f
boytrend0108 <boytrend@gmail.com>
2024-05-23 19:16:41 +0300

    change port

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
   server: {
-    port: 80,
+    port: 8080,
     strictPort: true,
     host: true,
-    origin: 'http://0.0.0.0:80',
+    origin: 'http://0.0.0.0:8080',
   },
 });
